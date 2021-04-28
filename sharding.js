3bb2973c08237cf25b526bf6ac7785f0ce55e572
DarkInk <rstonehouse66@gmail.com>
2021-04-28 14:25:16 -0500

    Fixed a quick bug

--- sharding.js
+++ sharding.js
@@ -7,6 +7,4 @@ const manager = new ShardingManager('./index.js', {
 
 manager.spawn();
 
-// The shardCreate event is emitted when a shard is created.
-// You can use it for something like logging shard launches.
 manager.on('shardCreate', (shard) => console.log(`Shard ${shard.id} launched`));
