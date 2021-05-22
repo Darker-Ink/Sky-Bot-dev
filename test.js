a269f662557ed8024317d07aa34e8fc7007f747b
DarkInk <45852757+Darker-Ink@users.noreply.github.com>
2021-05-22 03:12:06 +0000

    should fix the failing bug

--- test.js
+++ test.js
@@ -2,7 +2,11 @@ const Discord = require('discord.js');
 const fs = require('fs');
 const colors = require('colors')
 const event_handler = require('./event');
-const client = new Discord.Client();
+const client = new Discord.Client({
+     allowedMentions: { parse: ['users', 'roles'], repliedUser: true },
+   // intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_VOICE_STATES", "GUILD_MESSAGE_REACTIONS", "GUILD_MESSAGE_TYPING", "GUILD_PRESENCES", "GUILD_MEMBERS", "GUILD_BANS"]
+    intents: ["GUILDS", "GUILD_MEMBERS", "GUILD_BANS", "GUILD_EMOJIS", "GUILD_INTEGRATIONS", "GUILD_WEBHOOKS", "GUILD_INVITES", "GUILD_VOICE_STATES", "GUILD_PRESENCES", "GUILD_MESSAGES", "GUILD_MESSAGE_REACTIONS", "GUILD_MESSAGE_TYPING", "DIRECT_MESSAGES", "DIRECT_MESSAGE_REACTIONS", "DIRECT_MESSAGE_TYPING"]
+})
 
 function getDirectories() {
     return fs.readdirSync("./commands/").filter(function subFolders(file) {
