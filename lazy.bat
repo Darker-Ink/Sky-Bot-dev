9d18afcccb61e686e3f846d2c7ccd8a275832711
DarkInk <rstonehouse66@gmail.com>
2021-05-23 16:01:36 -0500

    commit from lazy.bat

--- lazy.bat
+++ lazy.bat
@@ -1,10 +1,10 @@
 @echo off && title lazy github push
 
 git pull
-choice /d y /t 5 > nul
+choice /d y /t 2 > nul
 git add .
-choice /d y /t 5 > nul
+choice /d y /t 2 > nul
 git commit -m "commit from lazy.bat"
-choice /d y /t 5 > nul
+choice /d y /t 2 > nul
 git push origin Master
-choice /d y /t 5 > nul
+choice /d y /t 2 > nul
