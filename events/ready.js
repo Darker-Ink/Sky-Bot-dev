e5e4302688a1230dddbfde7d47e95277f3a19573
DarkInk <rstonehouse66@gmail.com>
2021-05-24 11:10:36 -0500

    fixing a bug

--- events/ready.js
+++ events/ready.js
@@ -18,7 +18,7 @@ module.exports = {
 	]
 
 setInterval(() => {
-    exec("npx pm2 status", (error, stdout) => {
+    exec("npx pm2 status 0", (error, stdout) => {
     let result = (stdout || error);
     pm2stats.send("Auto Stats\n\n\`\`\`" + result + "\`\`\`\n\n" + `${time}`)
 })
