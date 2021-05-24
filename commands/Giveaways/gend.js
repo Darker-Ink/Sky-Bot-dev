96a37e8e34550d929065eee515325e8f99f09c7b
DarkInk <45852757+Darker-Ink@users.noreply.github.com>
2021-05-23 21:13:09 -0500

    Update gend.js

--- commands/Giveaways/gend.js
+++ commands/Giveaways/gend.js
@@ -4,6 +4,7 @@ module.exports = {
     name: 'gend',
     description: "Ends the giveaway",
     category: "Giveaway",
+    perms: "MANAGE_MESSAGES",
     run: async (client, message, args) => {
 
         let giveaway4 =
@@ -18,9 +19,6 @@ module.exports = {
 
 
         // If the member doesn't have enough permissions
-        if (!message.member.hasPermission('MANAGE_MESSAGES') && !message.member.roles.cache.some((r) => r.name === "Giveaways")) {
-            return message.channel.send(':x: You need to have the manage messages permissions to end giveaways.');
-        }
 
         // If no message ID or giveaway name is specified
         if (!args[0]) {
