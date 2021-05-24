74f328a129b514588116f30589e60f551ad9b7e2
DarkInk <45852757+Darker-Ink@users.noreply.github.com>
2021-05-23 21:14:00 -0500

    Update greroll.js

--- commands/Giveaways/greroll.js
+++ commands/Giveaways/greroll.js
@@ -17,10 +17,6 @@ module.exports = {
         // If no giveaway was found
         if (!giveaway4) return message.channel.send('Unable to find a giveaway for `' + args.join(' ') + '`.');
         // If the member doesn't have enough permissions
-        if (!message.member.hasPermission('MANAGE_MESSAGES') && !message.member.roles.cache.some((r) => r.name === "Giveaways")) {
-            return message.channel.send(':x: You need to have the manage messages permissions to reroll giveaways.');
-        }
-
         // If no message ID or giveaway name is specified
         if (!args[0]) {
             return message.channel.send(':x: You have to specify a valid message ID!');
