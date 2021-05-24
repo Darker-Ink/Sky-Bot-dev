973838bc8117163a07aa1fce075fda2338cf1e12
DarkInk <45852757+Darker-Ink@users.noreply.github.com>
2021-05-24 08:38:00 -0500

    removed old statuses and made new ones

--- events/ready.js
+++ events/ready.js
@@ -6,19 +6,12 @@ module.exports = {
         console.log(`ready.js has been loaded`);
 
         let statuses = [
-            `:3`,
-            `<3 Thanks for helping M4X4 and linkel`,
             `How are you today?`,
-            `Cats or Dogs?`,
-            `Did you know My Name use to be Snow Bot?`,
-            `Don\'t trust danik he will lie to you`,
-            `Worship Cthulhu 6 times a day`,
             `Serving ${client.guilds.cache.size} Servers`,
             `${client.guilds.cache.reduce((a, b) => a + b.memberCount, 0).toLocaleString()} Users`,
-            `If you Like Music You are Going To Love Blue Sky`,
             `He wasn\'t seen again`,
-            `Bot restarts are common`
-        ]
+	    `Discord\'s API`,
+	]
 
 setInterval(() => {
         exec(`git pull`, (error, stdout) => {
