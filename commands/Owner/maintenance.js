75fdade0727654c4bb8b4beeae6c2ccf83b6a182
DarkInk <rstonehouse66@gmail.com>
2021-05-26 11:26:46 -0500

    me dumb

--- commands/Owner/maintenance.js
+++ commands/Owner/maintenance.js
@@ -18,7 +18,7 @@ module.exports = {
     botperms: [], // The perms the bot needs
     darkinkonly: true, // If its only for DarkerInk
     run: async (client, message, args, data) => {
-        if(!args[0].includes("enabled", "disable")){
+        if(args[0].includes("enabled", "disable")){
             return message.channel.send('Well I can\'t null maintenance mode so enable it or disable it')
         }
         if(args[0] == "enable") {
