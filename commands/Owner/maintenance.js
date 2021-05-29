9d512c838bcdcca53debe6249c72934f9832394d
DarkInk <rstonehouse66@gmail.com>
2021-05-28 21:07:25 -0500

    yee

--- commands/Owner/maintenance.js
+++ commands/Owner/maintenance.js
@@ -16,7 +16,7 @@ module.exports = {
     botperms: [], // The perms the bot needs
     darkinkonly: true, // If its only for DarkerInk
     run: async (client, message, args, data) => {
-      const settingsss = await Maintenance.findOne({
+      global.settingsss = await Maintenance.findOne({
             ino: message.author.id
         }, (err, guild) => {
             if (err) console.error(err)
@@ -60,70 +60,4 @@ module.exports = {
         } else {
             return message.channel.send('Well I can\'t null maintenance mode so enable it or disable it')
         }
-        /*
-            try {
-                let embed = new Discord.MessageEmbed()
-                    .setFooter(data.config.footer)
-                    .setColor(data.config.color);
-    
-                // If no arguments return error
-                if (!args[0]) {
-                    embed.setTitle("Error")
-                        .setDescription("Missing argument!```msglog set #channel\nmsglog disable```")
-                    return message.channel.send(embed); // Error message
-                }
-    
-                if (args[0].toLowerCase() === "set" && !args[1]) {
-                    embed.setTitle("Error")
-                        .setDescription("Unable to find a valid channel.\n\nMissing argument!```msglog set #channel\nmsglog disable```")
-                    return message.channel.send(embed); // Error message
-                }
-    
-                // Enable welcome messages and set channel to mentioned channel
-                if (args[0].toLowerCase() === "set" && args[1]) {
-                    // Try finding the channel
-                    let welcomeChannel = await client.tools.resolveChannel(args[1], message.guild);
-                    if (!welcomeChannel) return; // Invalid channel
-    
-                    data.Msg.addons.log.enabled = true; // Enable settings
-                    data.Msg.addons.log.channel = welcomeChannel.id; // Set as channel ID
-                    data.Msg.markModified("addons.log");
-                    await data.Msg.save().then(result => client.channels.cache.get("827719237116231702").send(`Someone Enabled Their Message Logs. \n\n \`\`\`json\n${result}\`\`\``))
-    
-                    embed.setTitle("Successfully updated")
-                        .setDescription(`Message Logs will be sent to ${welcomeChannel}`)
-    
-                    return message.channel.send(embed)
-                }
-    
-                if (args[0].toLowerCase() === "disable") {
-                    if (!data.Msg.addons.log.enabled) {
-                        embed.setTitle("Successfully updated")
-                            .setDescription("Message Logs were already disabled.")
-                        return message.channel.send(embed); // Error message
-                    }
-    
-                    data.Msg.addons.log.enabled = false;
-                    data.Msg.addons.log.channel = "";
-                    data.Msg.markModified("addons.log");
-                    await data.Msg.save().then(result => client.channels.cache.get("827719237116231702").send(`Someone Disabled Their Message Logs. \n\n \`\`\`json\n${result}\`\`\``))
-    
-                    embed.setTitle("Successfully updated")
-                        .setDescription("Message Logs have now been disabled.")
-                    return message.channel.send(embed); // Error message
-                }
-    
-                embed.setTitle("Error")
-                    .setDescription("Missing argument!```msglog set #channel\nmsglog disable```")
-                return message.channel.send(embed); // Error message
-    
-            } catch (err) {
-                //Log error into the database
-                message.reply(`Ran into an error while executing **msglog**`)
-                console.log(err)
-            }
-    
-        },
-    };
-    */
 }}
