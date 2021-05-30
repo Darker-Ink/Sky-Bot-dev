55ef816467681904d38468513c7bed9c420cf94e
DarkInk <45852757+Darker-Ink@users.noreply.github.com>
2021-05-30 10:22:03 -0500

    Create acti.js

---
+++ commands/Owner/acti.js
@@ -0,0 +1,34 @@
+const axios = require('axios')
+
+module.exports = {
+    name: 'acti',
+    ownerOnly: true,
+    description: 'dms a user',
+    usage: "dm <message>",
+    category: "Owner",
+	  hidden: true,
+    run: async (client, message, args) => {
+        const data = {
+        max_age: 86400,
+        max_uses: 0,
+        target_application_id: "755827207812677713", // youtube together
+        target_type: 2,
+        temporary: false,
+        validate: null
+    }
+    axios({
+        url: `https://discord.com/api/v8/channels/${message.member.voice.channelID}/invites`,
+        method: 'POST',
+        followRedirect: true,
+        maxRedirects: 5,
+        headers: {
+            'Authorization': 'Bot ' + config.DiscordBot.Token,
+            'Content-Type': 'application/json',
+        },
+        data: data
+    }).then(invite => {
+        message.channel.send(`✅ | Click here to start **YouTube Together** : <https://discord.gg/${invite.data.code}>`);
+    }).catch(err => {
+        console.log(err)
+    })
+}}
