8e30c02683a5fe7eb8cf16f49af397d9f5eb9ca1
DarkInk <rstonehouse66@gmail.com>
2021-05-30 15:36:25 -0500

    tocket

--- commands/Owner/tocket.js
+++ commands/Owner/tocket.js
@@ -19,15 +19,26 @@ module.exports = {
             if (m.content.toLowerCase() === "confirm") {
                 message.channel.send("**Closing ticket.**", null).then(setTimeout(() => {
                     message.channel.delete()
+                    message.channel.messages.fetch().then(async (messages) => {
+                        const script = messages.array().reverse().map(m => `${m.author.tag}: ${m.attachments.size > 0 ? m.attachments.first().proxyURL : m.content}`).join('\n')
+    
+                        create([
+                    {
+                        content: script,
+                        language: "text",
+                    },
+                    ]).then(bin=> {
                     const Discord = require('discord.js') 
-                    const channel = client.channels.cache.get('846410364353511455')
-                    const embed = new Discord.MessageEmbed()
-                    .setColor('RED')
-                    .setTitle(`A Ticket Has Been Closed`)
-                    .setDescription(`User: <@!${message.author.id}>\n\n Has closed a Ticket: ${message.channel.name}`)
-                    .setFooter(message.author.id)
-                    .setTimestamp()
-                    channel.send(embed)
+                        const channel = client.channels.cache.get('848640177098063932') //OwO You need a channel ID I suggest making a new one :)
+                        const embed = new Discord.MessageEmbed()
+                        .setColor('RED')
+                        .setTitle(`A Ticket Has Been Closed`)
+                        .setDescription(`User: <@!${message.author.id}>\n\n Ticket Name: **${message.channel.name}**`)
+                        .setFooter(message.author.id)
+                        .setTimestamp()
+                        channel.send(`Transcript: ${bin.url}`, embed)
+                })
+            })
                 }, 5000))
             } else if (m.content.toLowerCase() === "cancel") {
                 message.channel.send('Closing ticket. __**Canceled**__ Ticket staying open.');
