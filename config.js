74a3c8a02181819c6048c8ddb42a01930a4a1684
Darker-Ink <rstonehouse66@gmail.com>
2021-05-30 03:22:42 +0000

    I'm going to eat you github

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     channel: "810967478536241243",
-    messageTemplate: "<@&810264106707583016> Hey Guys, **{author}** just now uploaded a New video **{title}**! Go check it Out!\n{url}",
+    messageTemplate: "Hey Guys, **{author}** just now uploaded a New video **{title}**! Go check it Out!\n{url}",
     channel_id: "UCU6yJp13DrT9cHiCCbBAziA",
     watchInterval: 30000
 }
