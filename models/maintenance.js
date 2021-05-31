83bbe04617df367b63479b2b6d3cdd7454c6a214
DarkInk <45852757+Darker-Ink@users.noreply.github.com>
2021-05-31 17:28:31 -0500

    Update maintenance.js

--- models/maintenance.js
+++ models/maintenance.js
@@ -4,6 +4,7 @@ const maintenance = mongoose.Schema({
 
     maintenance: {type: String, default: 'maintenance'},
     toggle: {type: String, default: 'false'},
+    reason: {type: String, default: ' '},
    
 })
 
