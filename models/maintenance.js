ad94b33bb73534f0f9ea7a7442a1bc87b77edf02
DarkInk <45852757+Darker-Ink@users.noreply.github.com>
2021-05-31 17:11:14 -0500

    Update maintenance.js

--- models/maintenance.js
+++ models/maintenance.js
@@ -1,10 +1,10 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
-const MaintenanceMode = mongoose.Schema({
-    _id: mongoose.Schema.Types.ObjectId,
-    ino: String,
-    reason: String,
-    enabled: String
-});
+const maintenance = mongoose.Schema({
 
-module.exports = mongoose.model('Maintenance', MaintenanceMode, 'maintenance');
+    maintenance: {type: String, default: 'maintenance'},
+    toggle: {type: String, default: 'false'},
+   
+})
+
+module.exports = mongoose.model('maintenance', maintenance)
