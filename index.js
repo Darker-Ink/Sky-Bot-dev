1d0e0e2c2ee76db7109b6f15d94365391cfea6f9
DarkInk <45852757+Darker-Ink@users.noreply.github.com>
2021-05-31 09:40:06 -0500

    Update index.js

--- index.js
+++ index.js
@@ -97,7 +97,6 @@ client.on('ready', () => {
 })
 
 client.login(process.env.token);
-const DarkerInk = client.users.cache.find(u => u.id === '379781622704111626').tag
 const status = (queue) =>
  `Volume: \`${queue.volume}%\` | Filter: \`${queue.filter || "Off"
   }\` | Loop: \`${queue.repeatMode
