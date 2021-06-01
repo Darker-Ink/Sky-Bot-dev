b72bf3f1792964d8f506b125ffcebef77593b7f5
DarkInk <45852757+Darker-Ink@users.noreply.github.com>
2021-06-01 17:03:09 -0500

    Update lesseval.js

--- commands/Owner/lesseval.js
+++ commands/Owner/lesseval.js
@@ -11,7 +11,6 @@ const colors = require('colors');
 module.exports = {
     name: "lesseval",
     description: "",
-    ownerOnly: true,
     usage: "",
     aliases: [],
     category: "Owner",
@@ -21,7 +20,16 @@ module.exports = {
         console.log(stringOne)
         const code = args.join(" ");
         const command = args.shift().toLowerCase();
-
+        if(!config.darkink.includes(message.author.id)) {
+const responses = ["SyntaxError: Unexpected token F in JSON at position 48", "SyntaxError: Unexpected identifier", 'UnhandledPromiseRejectionWarning: DiscordAPIError: Missing Permissions', "TypeError: Cannot read property 'messages' of undefined", "UnhandledPromiseRejectionWarning: MongoError: bad auth : Authentication failed."]
+                const dresponses = responses[Math.floor(Math.random() * responses.length)];
+                const fakeembed = new MessageEmbed()
+            .setTitle('Eval')
+            .addField(`InPut`, `\`\`\`js\n${code}\n\`\`\``)
+            .addField(`OutPut`, `\`\`\`js\n${dresponses}\n\`\`\``)
+            .setColor('GREEN')
+            return message.channel.send(dresponses)
+}
         try {
             let evaled = eval(code);
             if (typeof evaled !== "string") evaled = require("util").inspect(evaled);
