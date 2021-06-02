a8c2c55d17b6a0a3b14a056a8f476f071f9bf402
DarkInk <rstonehouse66@gmail.com>
2021-06-02 16:34:02 -0500

    f

--- index.js
+++ index.js
@@ -161,11 +161,10 @@ client.distube
     .on("searchCancel", message => message.channel.send(`${client.emotes.error} | Searching canceled`));
 
     client.on("guildCreate", async (guild) => {
-        const Guild = require('./schema.js')
-    const mongoose = require('mongoose');
     const config = require('./config/config.json')
     const joinlog = new Discord.WebhookClient(config.joinlogid, config.joinlogtoken);
-    /*
+    console.log(guild.id)
+   console.log(guild.name)
     global.settings = await Guild.findOne({
         guildID: guild.id
     }, (err, guild) => {
@@ -173,21 +172,20 @@ client.distube
         if (!guild) {
             const newGuild = new Guild({
                 _id: mongoose.Types.ObjectId(),
-                guildID: guild.id,
-                guildName: guild.name,
+                guildID: `${guild.id}`,
+                guildName: `${guild.name}`,
                 prefix: '!'
             })
     
             newGuild.save()
                 //sends a msg to the channel saying someone has been added to the database
-                .then(result => client.channels.cache.get("827719237116231702").send(`<@379781622704111626> Someone Has been Added to the Database. \n\n \`\`\`${result}\`\`\``))
+                .then(result => joinlog.get("827719237116231702").send(`<@379781622704111626> Someone Has been Added to the Database. \n\n \`\`\`${result}\`\`\``))
                 .catch(err => console.error(err));
     
             //used to stop a error
             return console.log('I wasn\'t here')
         }
     });
-    */
    console.log(guild.id)
    console.log(guild.name)
     })
