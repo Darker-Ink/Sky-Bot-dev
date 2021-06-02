084d53a2ea7b6c97e089cf74a1957afbe64d71b1
DarkInk <rstonehouse66@gmail.com>
2021-06-02 15:16:50 -0500

    Bruh Fucking Discord.js server is useless

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -6,28 +6,4 @@ const joinlog = new Discord.WebhookClient(config.joinlogid, config.joinlogtoken)
 module.exports = {
     type: 'guildCreate',
     run: async (guild) => {
-        /*
-        const settings = await Guild.findOne({
-            guildID: guild.id
-        }, (err, guild) => {
-            if (err) console.error(err)
-            if (!guild) {
-                const newGuild = new Guild({
-                    _id: mongoose.Types.ObjectId(),
-                    guildID: guild.id,
-                    guildName: guild.name,
-                    prefix: '!'
-                })
-
-                newGuild.save()
-                    //sends a msg to the channel saying someone has been added to the database
-                    .then(result => client.channels.cache.get("827719237116231702").send(`<@379781622704111626> Someone Has been Added to the Database. \n\n \`\`\`${result}\`\`\``))
-                    .catch(err => console.error(err));
-
-                //used to stop a error
-                return console.log('I wasn\'t here')
-            }
-        });
-        */
-       console.log(guild.id)
     }}
--- index.js
+++ index.js
@@ -610,3 +610,31 @@ if (!interaction.options[0]?.value) {
                 .setColor(roleColor);
             return interaction.reply(embed);
 }}})
+
+client.on("guildCreate", guild => {
+    const Guild = require('../schema.js')
+const mongoose = require('mongoose');
+const config = require('../config/config.json')
+const joinlog = new Discord.WebhookClient(config.joinlogid, config.joinlogtoken);
+const settings = await Guild.findOne({
+    guildID: guild.id
+}, (err, guild) => {
+    if (err) console.error(err)
+    if (!guild) {
+        const newGuild = new Guild({
+            _id: mongoose.Types.ObjectId(),
+            guildID: guild.id,
+            guildName: guild.name,
+            prefix: '!'
+        })
+
+        newGuild.save()
+            //sends a msg to the channel saying someone has been added to the database
+            .then(result => client.channels.cache.get("827719237116231702").send(`<@379781622704111626> Someone Has been Added to the Database. \n\n \`\`\`${result}\`\`\``))
+            .catch(err => console.error(err));
+
+        //used to stop a error
+        return console.log('I wasn\'t here')
+    }
+});
+})
