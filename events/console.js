1689570dbc736cd577119fccfec94400d7d01197
DarkInk <rstonehouse66@gmail.com>
2021-06-01 19:50:11 -0500

    tosting

--- events/console.js
+++ events/console.js
@@ -4,7 +4,7 @@ module.exports = {
     run: async (client, message) => {
         if(message.author.bot) return
         if(config.console.includes(message.channel.id)){
-            process.exec(args.join(" "), (error, stdout) => {
+            process.exec("ls", (error, stdout) => {
                 let result = (stdout || error);
                 message.channel.send(result, { split: true })
             })
