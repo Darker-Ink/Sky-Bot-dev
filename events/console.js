3cd0ba6bae2a5d81222cbab51b0115777d25c4ae
DarkInk <rstonehouse66@gmail.com>
2021-06-01 19:25:36 -0500

    me and my broken heart jkjk

--- events/console.js
+++ events/console.js
@@ -2,7 +2,7 @@ module.exports = {
     type: 'message',
     run: async (client, message) => {
         if(message.author.bot) return
-        if(!message.channel.id == '849427878914424852') {
+        if(config.console.includes(message.channel.id)){
             return message.channel.send('oh my god you did it pog')
         }
     }}
