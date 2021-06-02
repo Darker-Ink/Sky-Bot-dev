5d66abbe75e15f417f8efbc489971d6ad1678682
DarkInk <rstonehouse66@gmail.com>
2021-06-01 19:57:31 -0500

    there I made a way to talk in the channel

--- events/console.js
+++ events/console.js
@@ -3,10 +3,14 @@ module.exports = {
     type: 'message',
     run: async (client, message) => {
         if(message.author.bot) return
+        if(!config.owners.includes(message.author.id)) {
+            return
+        }
+        if(message.content.startsWith('>')) return
         if(config.console.includes(message.channel.id)){
             process.exec(`${message.content}`, (error, stdout) => {
                 let result = (stdout || error);
-                message.channel.send(result, { split: true })
+                message.channel.send(`\`\`\`\n${result}\n\`\`\``, { split: true })
             })
         }
     }}
