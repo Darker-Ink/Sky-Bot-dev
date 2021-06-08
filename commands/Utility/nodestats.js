aa9ffd3798068667ed2375e59c61e2aa8305815b
DarkInk <45852757+Darker-Ink@users.noreply.github.com>
2021-06-08 16:02:10 -0500

    Update nodestats.js

--- commands/Utility/nodestats.js
+++ commands/Utility/nodestats.js
@@ -23,5 +23,5 @@ module.exports = {
         if (!args[0] || args[0].toLowerCase() === "all" || args[0].toLowerCase() === "global") {
                 let nodes = await fetch("https://disease.sh/v3/covid-19/all")
                 nodes = await nodes.json()
-                return msg.edit(`${Node7.is_vm_online}`)
+                return msg.edit(`${nodes.Node7.is_vm_online}`)
         }}}
